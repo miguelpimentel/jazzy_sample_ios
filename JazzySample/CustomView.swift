9810c694bf5e359ee4d24db9d1d338870b8285c8
Miguel Pimentel <miguel.silva@tembici.com>
2020-06-28 09:43:11 -0300

    Minimal adjustments

---
+++ JazzySample/CustomView.swift
@@ -0,0 +1,19 @@
+//
+//  CustomView.swift
+//  JazzySample
+//
+//  Created by Miguel Pimentel on 28/06/20.
+//  Copyright © 2020 Miguel Pimentel. All rights reserved.
+//
+
+import UIKit
+
+/// View extensions
+public extension UIView {
+
+    /// Set background color
+    /// - Parameter color: UIColor
+    func setupLayout(_ color: UIColor) {
+        self.backgroundColor = color
+    }
+}
